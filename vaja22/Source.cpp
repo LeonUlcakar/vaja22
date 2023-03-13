3d71d0611f43d9e8cf824e51187e55305c547794
LeonUlcakar <leon.ulcakar@gmail.com>
2023-03-13 10:26:23 +0100

    fixed

--- vaja22/Source.cpp
+++ vaja22/Source.cpp
@@ -3,19 +3,21 @@
 #include <stdio.h>
 using namespace std;
 
-double square(int num);
+double square(int num, double resolution);
 
 int main() {
+	double resolution = 0.00001;
 	for (int i = 1; i < 1001; i++) {
-		cout << square(i) << endl;
+		//cout << square(i, resolution) << endl;
+		printf("%.5f\n", square(i, resolution));
 	}
 	return 0;
 }
 
-double square(int num) {
+double square(int num, double resolution) {
 	double root = 1;
 	double ort = 0;
-	while (root != ort) {
+	while ((root - resolution) > ort) {
 		ort = root;
 		root = ((num / root) + root) / 2;
 	}
